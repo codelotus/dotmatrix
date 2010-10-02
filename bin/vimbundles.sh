88e8b3380c06dc8c5a27834e5081112db0586d04
Brian Ehmann <behmann@gmail.com>
2010-10-02 10:48:13 -0400

    added auto install of fuzzyfinder vim plugin

--- bin/vimbundles.sh
+++ bin/vimbundles.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.vimbundles
 cd ~/.vimbundles
 
-get_bundle() {
+get_github_bundle() {
   (
   if [ -d "$2" ]; then
     echo "Updating $1's $2"
@@ -13,31 +13,47 @@ get_bundle() {
   )
 }
 
-get_bundle alowe vim-slurper
-get_bundle duff vim-bufonly
-get_bundle godlygeek tabular
-get_bundle leshill vim-json
-get_bundle mileszs ack.vim
-get_bundle pangloss vim-javascript
-get_bundle scrooloose nerdcommenter
-get_bundle therubymug vim-pyte
-get_bundle tpope vim-abolish
-get_bundle tpope vim-cucumber
-get_bundle tpope vim-endwise
-get_bundle tpope vim-fugitive
-get_bundle tpope vim-git
-get_bundle tpope vim-haml
-get_bundle tpope vim-markdown
-get_bundle tpope vim-pathogen
-get_bundle tpope vim-rake
-get_bundle tpope vim-ragtag
-get_bundle tpope vim-rails
-get_bundle tpope vim-repeat
-get_bundle tpope vim-speeddating
-get_bundle tpope vim-surround
-get_bundle tpope vim-unimpaired
-get_bundle tpope vim-vividchalk
-get_bundle vim-ruby vim-ruby
-get_bundle wgibbs vim-irblack
-get_bundle scrooloose nerdtree
-get_bundle sjbach lusty
+get_bitbucket_bundle() {
+  (
+  if [ -d "$2" ]; then
+    echo "Updating $1's $2"
+    cd "$2"
+    hg pull
+    hg update
+  else
+    hg clone "http://bitbucket.org/$1/$2"
+  fi
+  )
+}
+
+get_github_bundle alowe vim-slurper
+get_github_bundle duff vim-bufonly
+get_github_bundle godlygeek tabular
+get_github_bundle leshill vim-json
+get_github_bundle mileszs ack.vim
+get_github_bundle pangloss vim-javascript
+get_github_bundle scrooloose nerdcommenter
+get_github_bundle therubymug vim-pyte
+get_github_bundle tpope vim-abolish
+get_github_bundle tpope vim-cucumber
+get_github_bundle tpope vim-endwise
+get_github_bundle tpope vim-fugitive
+get_github_bundle tpope vim-git
+get_github_bundle tpope vim-haml
+get_github_bundle tpope vim-markdown
+get_github_bundle tpope vim-pathogen
+get_github_bundle tpope vim-rake
+get_github_bundle tpope vim-ragtag
+get_github_bundle tpope vim-rails
+get_github_bundle tpope vim-repeat
+get_github_bundle tpope vim-speeddating
+get_github_bundle tpope vim-surround
+get_github_bundle tpope vim-unimpaired
+get_github_bundle tpope vim-vividchalk
+get_github_bundle vim-ruby vim-ruby
+get_github_bundle wgibbs vim-irblack
+get_github_bundle scrooloose nerdtree
+get_github_bundle sjbach lusty
+get_bitbucket_bundle ns9tks vim-l9
+get_bitbucket_bundle ns9tks vim-fuzzyfinder
+
