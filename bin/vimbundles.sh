b3e552822f7b97dfeab955d727903111905f5af2
Brian Ehmann <behmann@gmail.com>
2012-02-24 22:54:26 -0700

    jslint over js hint

--- bin/vimbundles.sh
+++ bin/vimbundles.sh
@@ -56,7 +56,8 @@ get_github_bundle taq vim-rspec
 get_github_bundle wgibbs vim-irblack
 get_github_bundle scrooloose nerdtree
 get_github_bundle sjbach lusty
-get_github_bundle wookiehangover jshint.vim
+#get_github_bundle wookiehangover jshint.vim
+get_github_bundle hallettj jslint.vim
 get_github_bundle kchmck vim-coffee-script
 get_github_bundle statianzo vim-jade
 get_github_bundle derekwyatt vim-scala
